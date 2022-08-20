54624b9da9ccafdc415fd0cd367086592ec3ac04
阿波次的额佛歌 <453096053@qq.com>
2022-08-20 14:12:37 +0800

    项目初始化

---
+++ src/router/modules/login.js
@@ -0,0 +1,8 @@
+import Login from '../../views/login/login.vue'
+const routes = [
+    {
+        path:'/login',
+        component:Login
+    }
+]
+export default routes
---
+++ src/views/login/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <h2>用户管理系统</h2>
+    <el-card class="box-card">
+      <el-tabs v-model="activeName" @tab-click="handleClick" :stretch="true">
+        <el-tab-pane label="密码登录" name="first">密码登录</el-tab-pane>
+        <el-tab-pane label="短信登录" name="second">短信登录</el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import '../../assets/css/login.scss'
+export default {
+  data() {
+    return {
+      activeName: "second",
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      //   console.log(tab, event);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
